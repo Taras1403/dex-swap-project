296f1d19450a64a38cb5edd3515dc09d21b4bb04
Mac <mac@MacBook-Air.local>
2025-08-17 19:26:51 +0300

    Add mint function to TokenA contract

--- backend/contracts/TokenA.sol
+++ backend/contracts/TokenA.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TokenA is ERC20 {
-    constructor() ERC20("Token A", "TKA") {
-        _mint(msg.sender, 1000000 * 10 ** 18);
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
     }
 }
