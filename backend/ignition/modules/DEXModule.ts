75cb53541fd759c6f2b12c87a5ec75be49e14c65
Mac <mac@MacBook-Air.local>
2025-08-17 19:28:45 +0300

    Pass arguments to TokenA and TokenB constructors

--- backend/ignition/modules/DEXModule.ts
+++ backend/ignition/modules/DEXModule.ts
@@ -1,16 +1,11 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 const DEXModule = buildModule("DEXModule", (m) => {
-  // Deploying our first token
-  const tokenA = m.contract("TokenA");
+  const tokenA = m.contract("TokenA", ["Token A", "TKA"]);
+  const tokenB = m.contract("TokenB", ["Token B", "TKB"]);
 
-  // Deploy our second token
-  const tokenB = m.contract("TokenB");
-
-  // Deploy the DEX contract, passing the token addresses
   const dex = m.contract("DEX", [tokenA, tokenB]);
 
-  // Return the expanded contracts
   return { tokenA, tokenB, dex };
 });
 
