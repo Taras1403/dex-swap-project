12733389398ed78fbad6f310b8a54b8c77337c9f
Mac <mac@MacBook-Air.local>
2025-08-19 15:31:21 +0300

    Add logo

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -5,11 +5,11 @@ import { ConnectKitButton } from 'connectkit';
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 w-full flex justify-between items-center px-4 py-4 bg-gray-900 text-white z-50">
-      {/* Логотип зліва */}
-      <div className="flex items-center">
+    <header className="fixed h-16 top-0 left-0 w-full flex justify-between items-center px-4 py-4 bg-gray-900 text-white z-50">
+      {/* logo*/}
+      <div className="flex items-center relative w-20 h-full">
         <a href="/">
-          <Image src="/ваш-шлях-до-лого.svg" alt="Логотип" width={100} height={50} />
+          <Image src="/logo.png" alt="logo" width={60} height={20} className="max-h-full" />
         </a>
       </div>
 
