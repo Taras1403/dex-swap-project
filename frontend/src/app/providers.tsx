53c72e577fcafc2ce7ddc2140e655bc19d88df27
Mac <mac@MacBook-Air.local>
2025-08-19 13:23:11 +0300

    Moved the provider from page.tsx to providers.tsx

---
+++ frontend/src/app/providers.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import React from 'react';
+import { WagmiProvider } from 'wagmi';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ConnectKitProvider } from 'connectkit';
+import { config } from './wagmi';
+
+const queryClient = new QueryClient();
+
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectKitProvider>{children}</ConnectKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
