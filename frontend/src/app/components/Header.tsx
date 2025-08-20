b6ba264cd05a64c8ce9831fdbf56bb8eef3ac88f
Mac <mac@MacBook-Air.local>
2025-08-20 17:28:16 +0300

    Tab centering

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -2,24 +2,25 @@
 
 import Image from 'next/image';
 import { ConnectKitButton } from 'connectkit';
+import ThemeSwitcher from './ThemeSwitcher';
 
 export default function Header() {
   return (
-    <header className="fixed h-16 top-0 left-0 w-full flex justify-between items-center px-4 py-4 bg-gray-900 text-white z-50">
-      {/* logo*/}
-      <div className="flex items-center relative w-20 h-full">
+    <header className="fixed h-16 top-0 left-0 w-full flex justify-between items-center px-4 py-4 bg-[#e0e5ec] text-black dark:bg-[#1a202c] dark:text-white z-50">
+      <div className="flex-1 flex items-center relative w-20 h-full">
         <a href="/">
           <Image src="/logo.png" alt="logo" width={60} height={20} className="max-h-full" />
         </a>
       </div>
 
-      <nav className="flex items-center space-x-4">
-        <button className="text-gray-300 hover:text-white">Button 1</button>
-        <button className="text-gray-300 hover:text-white">Button 2</button>
-        <button className="text-gray-300 hover:text-white">Button 3</button>
+      <nav className="flex items-center space-x-4 md:mb-0">
+        <button className="dark:text-white text-black hover:text-white">Swap</button>
+        <button className="dark:text-white text-black hover:text-white">Transaction</button>
+        <button className="dark:text-white text-black hover:text-white">More</button>
       </nav>
 
-      <div>
+      <div className="flex-1 flex justify-center md:justify-end space-x-4">
+        <ThemeSwitcher />
         <ConnectKitButton />
       </div>
     </header>
