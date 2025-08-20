57f5f34e0aa960e741ce4f11d57bd9f73e4514fd
Mac <mac@MacBook-Air.local>
2025-08-20 17:30:44 +0300

    Centering the Dex interface

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from 'next/font/google';
 import { Geist, Geist_Mono } from "next/font/google";
 import Header from './components/Header';
+import Footer from './components/Footer';
 import { Providers } from './providers';
 
 import "./globals.css";
@@ -30,10 +31,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`} >
+      <body className={`flex flex-col min-h-screen bg-[#e0e5ec] dark:bg-[#1a202c] ${geistSans.variable} ${geistMono.variable} antialiased`} >
         <Providers>
           <Header />
-          <main>{children}</main>
+            <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-center">{children}</main>
+          <Footer />
         </Providers>
       </body>
     </html>
